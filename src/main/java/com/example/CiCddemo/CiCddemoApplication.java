f50826c67cb8b6f300c1e99dfd103fc949c1c9fb
chvijayalakshmi <challavijayalakshmi8@gmail.com>
2023-04-14 16:18:44 +0200

    Initial commit

---
+++ src/main/java/com/example/CiCddemo/CiCddemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CiCddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CiCddemo/CiCddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CiCddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
